ebc8cae75ffb3ac69135f3816c8b267bbb821645
Rayon0315 <1753368089@qq.com>
2025-11-09 09:20:47 +0800

    fixed 001

---
+++ myTrial/RealNumberMatrix.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+class RealNumber {
+private:
+    int A, B; // x = A/B
+
+public:
+    RealNumber (int A = 0, int B = 1) {}
+};
+
+int main() {
+}
